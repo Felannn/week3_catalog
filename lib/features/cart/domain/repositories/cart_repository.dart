4655366eeac083f23b21442c26f3986e675cc0e3
Felannn <felanyoga64@gmail.com>
2026-03-05 07:01:17 +0700

    bikin kontrak method

--- lib/features/cart/domain/repositories/cart_repository.dart
+++ lib/features/cart/domain/repositories/cart_repository.dart
@@ -1,3 +1,8 @@
-class CartRepository {
-  
+import 'package:week3_catalog/features/cart/domain/entities/product.dart';
+
+abstract class CartRepository {
+  List<Product> getCartItems();
+  void addItem(Product product);
+  void removeAll();
+  bool isItemInCart(String productId);
 }
