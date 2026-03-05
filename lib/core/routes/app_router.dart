c837ad2ecd5d81d3c9058a3fa7b672b4fa8fdc36
Felannn <felanyoga64@gmail.com>
2026-03-05 07:15:12 +0700

    benerin app router

--- lib/core/routes/app_router.dart
+++ lib/core/routes/app_router.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:week3_catalog/features/cart/presentation/pages/cart_page.dart';
+import 'package:week3_catalog/features/cart/presentation/pages/catalog_page.dart';
 
 class AppRouter {
   static const String catalog = '/';
